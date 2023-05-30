7e4e3ba7b77adcbf488af9a9e0b1adaf1088546f
aqua5432 <93812121+aqua5432@users.noreply.github.com>
2023-05-30 15:59:54 +0900

    Renewal ErrorCommand

--- src/main/java/cli/commands/error/ErrorCommand.java
+++ src/main/java/cli/commands/error/ErrorCommand.java
@@ -2,7 +2,7 @@ package cli.commands.error;
 
 import cli.utils.Logger;
 
-public class HelloCommand implements Runnable {
+public class ErrorCommand implements Runnable {
   @Override
   public void run() {
     Logger.success("This is not a poke command. See 'poke help'.");
