8be5ad2c182fdeff2ab225e9df73eabbe5e6e3ef
Tomoki-13 <134577377+Tomoki-13@users.noreply.github.com>
2023-06-01 15:40:26 +0900

    機能案の提案 #4を質問者にかわって作成 #38

--- src/main/java/cli/CLI.java
+++ src/main/java/cli/CLI.java
@@ -17,21 +17,33 @@ public class CLI implements Runnable {
       // コマンドを格納(poke get なら get)
       String command = args[0];
       // オプション格納(poke get 10なら 10)
-      String option = null;
+      String option1 = null;
+// オプション格納(poke get 10 20なら 20)
+      String option2 = null;
 
       // オプションが存在する時だけ変数に入れる
-      if (args.length == 2) {
-        option = args[1];
+      if (args.length >= 2) {
+        option1 = args[1];
+      }
+
+      if (args.length >= 3) {
+        option2 = args[2];
       }
 
       // コマンドごとに処理を分岐
-      if (option != null && command.equals("get")) {
-        int limit = Integer.parseInt(option);
+      if (option1 != null && option2 == null && command.equals("get")) {
+        int limit = Integer.parseInt(option1);
         new GetPokeNameList(limit).run();
       }
+      //getコマンドで2つ目の数値があった場合の処理を追加
+      if (option1 != null && option2 != null && command.equals("get")) {
+        int start  = Integer.parseInt(option1);
+        int limit = Integer.parseInt(option2);
+        new GetPokeNameList(start, limit).run();
+      }
       
-      if (option != null && command.equals("status")) {
-        String name = option;
+      if (option1 != null && option2 == null && command.equals("status")) {
+        String name = option1;
         new GetPokeStatus(name).run();
       }
       
